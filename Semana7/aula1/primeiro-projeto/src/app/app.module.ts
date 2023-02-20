f0c810e294b7f1af0dd4437578f0a4c85016f568
Daniele Tavares Lobo <danitavares.dev@gmail.com>
2023-02-20 19:24:19 -0300

    adiciona exemplos de aula semana 7

---
+++ Semana7/aula1/primeiro-projeto/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GaleriaComponent } from '../components/galeria/galeria.component';
+import {TesteComponent } from '../components/teste/teste.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GaleriaComponent,
+    TesteComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
